205a8512f213a52d9e0c29955450c54aa54a8847
Chris James <christopherjames.judge@gmail.com>
2025-06-24 13:47:58 -0600

    Update Flight_Project_App.py

--- Flight_Project_App.py
+++ Flight_Project_App.py
@@ -598,7 +598,7 @@ elif(app_mode=="About"):
     air travel smarter, smoother, and more enjoyable for everyone, one flight at a time.""")
     # Write about Meeting the team
     st.header("Meet the Team")
-    image_path2 = "Users/chrisjames/Documents/Python /Flights Project.jpeg" 
+    image_path2 = "https://github.com/chriskjames/Flight-Project/blob/main/Baby_Chris.jpeg" 
     st.image(image_path2)
     st.write("Chris James | CEO")
     
